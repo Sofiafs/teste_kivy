41ba5b82aa1894e2bc88476e3dbb37cce8b275a9
Sofiafs <sofiasantosf011@gmail.com>
2024-04-30 17:22:05 -0300

    quinto commit

---
+++ gridlayout.py
@@ -0,0 +1,10 @@
+from kivy.uix.gridlayout import GridLayout
+
+grid_layout =GridLayout(cols=2)
+
+grid_layout.add_widget()
+grid_layout.add_widget()
+
+grid_layout.spacing=[10, 10]
+grid_layout.padding=[20, 20]
+
